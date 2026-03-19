2333b358d957898dafa0470a46d32741dfb45f57
Harsh Vardhan <harsh.vardhanp0901@gmail.com>
2026-03-20 01:49:36 +0530

    inggest

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -7,7 +7,7 @@ export const inngest = new Inngest({ id: "zingle-app" });
 //ingest func to save user data to a database
 const syncUserCreation=inngest.createFunction(
     {id:'sync-user-from-clerk'},
-    {event:'clerk/user.created'},
+    {event:'user.created'},
     async({event})=>{
         const {id,first_name,last_name,email_addresses,image_url}=event.data
         let username=email_addresses[0].email_address.split('@')[0]
@@ -25,8 +25,8 @@ const syncUserCreation=inngest.createFunction(
             profile_picture:image_url,
             username
         }
-        let userIn=await User.create(userData)
-        console.log(userIn);
+       await User.create(userData)
+        // console.log(userIn);
         
     }
 )
@@ -34,7 +34,7 @@ const syncUserCreation=inngest.createFunction(
 //ingest function to update user in the database
 const syncUserUpdation=inngest.createFunction(
     {id:'update-user-from-clerk'},
-    {event:'clerk/user.updated'},
+    {event:'user.updated'},
     async({event})=>{
         const {id,first_name,last_name,email_addresses,image_url}=event.data
       
@@ -42,20 +42,20 @@ const syncUserUpdation=inngest.createFunction(
         email:email_addresses[0].email.address,
         full_name:first_name+" "+last_name,
       }
-        let userUpd=await User.findIdAndUpdate(id,updateUserData)
-        console.log(userUpd);
+        await User.findIdAndUpdate(id,updateUserData)
+        
     }
 )
 
 //ingest function to delete user in the database
 const syncUserDeletion=inngest.createFunction(
     {id:'delete-user-from-clerk'},
-    {event:'clerk/user.deleted'},
+    {event:'user.deleted'},
     async({event})=>{
         const {id}=event.data
       
-        let userDel=await User.findIdAndDelete(id)
-        console.log(userDel);
+        await User.findIdAndDelete(id)
+        
 
     }
 )
