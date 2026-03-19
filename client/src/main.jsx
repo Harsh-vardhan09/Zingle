cc5c0dc70115a5b360f94f865bce877106f7d299
Harsh Vardhan <harsh.vardhanp0901@gmail.com>
2026-03-20 01:49:36 +0530

    Inngest & clerk auth:fix 20/03/26

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Meta } from "react-router-dom";
 import { ClerkProvider } from "@clerk/react";
 
+const PUBLISAHBLE_KEY=import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+if(!PUBLISAHBLE_KEY){
+  throw new error('missing publishable key')
+}
+
 createRoot(document.getElementById("root")).render(
-  <ClerkProvider>
+  <ClerkProvider publishableKey={PUBLISAHBLE_KEY}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
--- server/server.js
+++ server/server.js
@@ -6,7 +6,8 @@ import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
 const app=express();
 await connectDb();
-const PORT=process.env.PORT| '8080';
+const PORT=process.env.PORT|| '8080';
+// const Development=process.env.NODE;
 
 app.use(express.json());
 app.use(cors())
@@ -16,9 +17,12 @@ app.get('/',(req,res)=>{
     res.send("server is running")
 })
 
+
 app.use('/api/inngest',serve({ client: inngest, functions }))
 
 app.listen(PORT,()=>{
     console.log(`server is running at http://localhost:${PORT}`);
     
 })
+
+
